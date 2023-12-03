a1906dab7ae338b33a40ff11126cc326326e149f
Zahid Hasan <zahidhasan065@gmail.com>
2023-12-03 16:55:33 +0600

    updated html

--- index.js
+++ index.js
@@ -1,97 +1,149 @@
 const axios = require('axios');
 
+// const URL = 'http://localhost:5000/create-completion';
+const URL = 'https://ersa-stg.programming-hero.com/create-completion';
+
 const requestsData = [
   {
-    url: 'http://localhost:5000/create-completion',
-    body: {
-      question: 'give me a php async example',
-      userId: '1234',
-    },
-  },
-  {
-    url: 'http://localhost:5000/create-completion',
+    url: URL,
     body: {
       question: 'give me a java async example',
       userId: '123',
     },
   },
   {
-    url: 'http://localhost:5000/create-completion',
+    url: URL,
     body: {
-      question: 'give me a javascript async example',
-      userId: '12',
+      question: 'give me a python async example',
+      userId: '1234',
     },
   },
   {
-    url: 'http://localhost:5000/create-completion',
+    url: URL,
     body: {
-      question: 'give me a javascript async example',
-      userId: '123111',
+      question: 'give me a ruby async example',
+      userId: '12345',
     },
   },
   {
-    url: 'http://localhost:5000/create-completion',
-    body: {
-      question: 'give me a php async example',
-      userId: '123466',
-    },
-  },
-  {
-    url: 'http://localhost:5000/create-completion',
+    url: URL,
     body: {
       question: 'give me a java async example',
-      userId: '123cfgg',
+      userId: '123456',
     },
   },
   {
-    url: 'http://localhost:5000/create-completion',
+    url: URL,
     body: {
-      question: 'give me a javascript async example',
-      userId: '1gfg2',
+      question: 'give me a python async example',
+      userId: '1234567',
     },
   },
   {
-    url: 'http://localhost:5000/create-completion',
+    url: URL,
     body: {
-      question: 'give me a javascript async example',
-      userId: '123cggccg111',
+      question: 'give me a ruby async example',
+      userId: '12345678',
     },
   },
   {
-    url: 'http://localhost:5000/create-completion',
+    url: URL,
     body: {
-      question: 'give me a javascript async example',
-      userId: '123111',
+      question: 'give me a ruby async example',
+      userId: '123456789',
     },
   },
-  {
-    url: 'http://localhost:5000/create-completion',
-    body: {
-      question: 'give me a php async example',
-      userId: '123466',
-    },
-  },
-  {
-    url: 'http://localhost:5000/create-completion',
-    body: {
-      question: 'give me a java async example',
-      userId: '123cfgg',
-    },
-  },
-  {
-    url: 'http://localhost:5000/create-completion',
-    body: {
-      question: 'give me a javascript async example',
-      userId: '1gfg2',
-    },
-  },
-  {
-    url: 'http://localhost:5000/create-completion',
-    body: {
-      question: 'give me a javascript async example',
-      userId: '123cggccg111',
-    },
-  },
+  //   {
+  //     url: URL,
+  //     body: {
+  //       question: 'give me a bash loop example',
+  //       userId: '123456',
+  //     },
+  //   },
+  //   {
+  //     url: URL,
+  //     body: {
+  //       question: 'hi',
+  //       userId: 'aaaaa',
+  //     },
+  //   },
+  //   {
+  //     url: URL,
+  //     body: {
+  //       question: 'hello',
+  //       userId: 'bbbbb',
+  //     },
+  //   },
+  //   {
+  //     url: URL,
+  //     body: {
+  //       question: 'give me a php async example',
+  //       userId: '1234',
+  //     },
+  //   },
+  //   {
+  //     url: URL,
+  //     body: {
+  //       question: 'give me a java async example',
+  //       userId: '123',
+  //     },
+  //   },
+  //   {
+  //     url: URL,
+  //     body: {
+  //       question: 'give me a php async example',
+  //       userId: '1234',
+  //     },
+  //   },
+  //   {
+  //     url: URL,
+  //     body: {
+  //       question: 'give me a java async example',
+  //       userId: '123',
+  //     },
+  //   },
+  //   {
+  //     url: URL,
+  //     body: {
+  //       question: 'give me a php async example',
+  //       userId: '1234',
+  //     },
+  //   },
+  //   {
+  //     url: URL,
+  //     body: {
+  //       question: 'give me a java async example',
+  //       userId: '123',
+  //     },
+  //   },
+  //   {
+  //     url: URL,
+  //     body: {
+  //       question: 'give me a php async example',
+  //       userId: '1234',
+  //     },
+  //   },
+  //   {
+  //     url: URL,
+  //     body: {
+  //       question: 'give me a java async example',
+  //       userId: '123',
+  //     },
+  //   },
+  //   {
+  //     url: URL,
+  //     body: {
+  //       question: 'give me a php async example',
+  //       userId: '1234',
+  //     },
+  //   },
+  //   {
+  //     url: URL,
+  //     body: {
+  //       question: 'give me a java async example',
+  //       userId: '123',
+  //     },
+  //   },
 ];
 
 // const requestsData = [
@@ -109,13 +161,13 @@ const requestsData = [
 //       userId: '123',
 //     },
 //   },
-//   {
-//     url: 'http://localhost:5000/test',
-//     body: {
-//       question: 'give me a php async example',
-//       userId: '12',
-//     },
-//   },
+//   //   {
+//   //     url: 'http://localhost:5000/test',
+//   //     body: {
+//   //       question: 'give me a php async example',
+//   //       userId: '12',
+//   //     },
+//   //   },
 // ];
 
 const requests = requestsData.map((requestData) =>
